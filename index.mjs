8b1b6985a0c113ba762f48d727ec92f58c282c6d
Ni <daisytsai11336@gmail.com>
2024-03-12 12:00:22 +0800

    20240312-express10

---
+++ index.mjs
@@ -0,0 +1,19 @@
+import epress from "express";
+import {resolve} from "path";
+const __dirname = import.meta.dirname;
+
+const app = epress();
+app.use(express.static(resolve(__dirname, "public")));
+app.use("/bootstrap", express.static(resolve(__dirname, "node_modules/bootstrap/dist")))
+app.use("/jquery", express.static(resolve(__dirname, "node_modules/jquery/dist")))
+app.use('/fontawesome', express.static(resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+
+
+app.get("/", (req, res) => {
+    res.send("網頁首頁");
+})
+
+
+app.listen(3000, () => {
+    console.log("伺服器啟動於 http://localhost:3000");
+})
